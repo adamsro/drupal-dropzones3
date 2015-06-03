509e8b74b98d4e41f3534f55397ebf411c0ff3f7
Marshall Adams <rma.kona@gmail.com>
2015-06-02 21:46:23 -0700

    filesize and extension validation on notify

--- dropzones3-init.js
+++ dropzones3-init.js
@@ -2,7 +2,6 @@
   Drupal.behaviors.dzs3 = {
     attach: function(context, settings) {
       $.each(settings.dzs3, function(selector) {
-        settings.dzs3[selector].validation.maxFilesize = 1000 * 100; // 100 GB
         new DropzoneS3(selector, settings.dzs3[selector]);
       });
     }
