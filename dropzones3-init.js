4656d3b2ca48c3803c5238171d2bee3cc136f148
Marshall Adams <rma.kona@gmail.com>
2015-06-07 19:39:54 -0700

    Saves the first file uploaded at least

--- dropzones3-init.js
+++ dropzones3-init.js
@@ -3,24 +3,28 @@
     attach: function(context, settings) {
       $.each(settings.dzs3, function(selector) {
         $(selector, context).once('dzs3', function() {
-          var template = document.querySelector(settings.dzs3[selector].previewTemplate);
+          var completed = 0,
+              template = document.querySelector(settings.dzs3[selector].previewTemplate);
           settings.dzs3[selector].previewTemplate = template.innerText;
 
           // Custom notify function called from emitter in DropzoneS3.prototype.finishUpload.
           settings.dzs3[selector].notify = function(file, done) {
-            var xhr = new XMLHttpRequest(),
-              field = this.options.drupalElementName + "[" + (this.files.length - 1) + "]";
+            var _this = this,
+              xhr = new XMLHttpRequest(),
+              field = this.options.drupalElementParents + "[" + completed + "]";
               formData = new FormData(document.getElementById(this.options.drupalFormId));
 
-              formData.append(field + "[name]", file.name);
-              formData.append(field + "[uri]", file.upload.auth.key);
+              formData.append(field + "[filename]", file.name);
+              formData.append(field + "[filesize]", file.size);
+              formData.append(field + "[key]", file.upload.auth.key);
               formData.append(field + "[fid]", 0);
               formData.append(field + "[display]", 1);
-              formData.append(field + "[_weight]", this.files.length - 1);
+              formData.append(field + "[_weight]", completed);
 
             xhr.onload = function() {
             if (xhr.status / 100 == 2) {
-              done();
+              completed++;
+              done(file);
             } else if (xhr.status / 100 == 5) {
               // Hopefully a temporary server error
               return _this._recoverableError(file, xhr);
