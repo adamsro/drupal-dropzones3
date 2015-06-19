46e0ec5336a8afce8f3abba5147fe075649900a5
Marshall Adams <rma.kona@gmail.com>
2015-06-19 13:07:03 -0700

    Remove notify fuction. value callback works in loop

--- dropzones3-init.js
+++ dropzones3-init.js
@@ -5,27 +5,24 @@
         $(selector, context).once('dzs3', function() {
           var completed = 0,
             template = document.querySelector(settings.dzs3[selector].previewTemplate);
+
           settings.dzs3[selector].previewTemplate = template.innerText;
 
-          // Add the fid so file is associated with form.
-          settings.dzs3[selector].success = function(file) {
+          settings.dzs3[selector].filesigned = function(file, auth, done) {
             var _ref0 = this.options.drupal.elementParents.slice(),
               field = _ref0.shift() + "[" + _ref0.join("][") + "][" + file.delta + "]";
 
-            if (file.previewElement) {
-              file.previewElement.classList.add("dzs3-success");
-            }
-
-            var fid = file.previewElement.querySelectorAll("[data-drupal-uploadid]");
-            for (var i = fid.length - 1; i >= 0; i--) {
-              fid[i].name = field + "[uploadid]";
-              fid[i].value = file.upload.auth.fid;
+            var key = file.previewElement.querySelectorAll("[data-drupal-key]");
+            for (var i = key.length - 1; i >= 0; i--) {
+              key[i].name = field + "[key]";
+              key[i].value = auth.key;
             }
             var filename = file.previewElement.querySelectorAll("[data-drupal-filename]");
             for (var j = filename.length - 1; j >= 0; j--) {
               filename[j].name = field + "[filename]";
               filename[j].value = file.name;
             }
+            done();
           };
           new DropzoneS3(selector, settings.dzs3[selector]);
         });
