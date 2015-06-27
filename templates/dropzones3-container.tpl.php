b30fec692ccf7ec54a15f8b26a52b5b496b5d6c8
Marshall Adams <rma.kona@gmail.com>
2015-06-26 20:42:07 -0700

    Use minified dropzone. Dont print wrapping el

--- templates/dropzones3-container.tpl.php
+++ templates/dropzones3-container.tpl.php
@@ -8,9 +8,7 @@
   <div class="dzs3-message">
   <?php print $element['#message']; ?>
   </div>
-  <span class="dzs3-previewlist">
-    <?php print drupal_render_children($element); ?>
-  </span>
+  <?php print drupal_render_children($element); ?>
 </div>
 <noscript><div class="dzs3-noscript"><?php print t('File uploader requires JavaScript be enabled.'); ?></div></noscript>
 <?php print $element['#description']; ?>
