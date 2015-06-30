9c0faa730a41ccb68a40c78699cbd32931c46396
Marshall Adams <rma.kona@gmail.com>
2015-06-30 13:19:01 -0700

    Give element a tabindex

--- templates/dropzones3-container.tpl.php
+++ templates/dropzones3-container.tpl.php
@@ -4,7 +4,7 @@
  * Template file for Dropzone S3 Widget.
  */
 ?>
-<div id="<?php print $element['#id']; ?>" <?php print drupal_attributes($element['#wrapper_attributes']); ?>>
+<div id="<?php print $element['#id']; ?>" tabindex="0" <?php print drupal_attributes($element['#wrapper_attributes']); ?>>
   <?php print drupal_render_children($element); ?>
 </div>
 <noscript><div class="dzs3-noscript"><?php print t('File uploader requires JavaScript be enabled.'); ?></div></noscript>
